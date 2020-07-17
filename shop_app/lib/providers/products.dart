94d99e143369b93659395a807ce5c27687889f77
Ronald Castillo <ronaldcastillo789@gmail.com>
2020-07-17 19:23:53 -0400

    implement Future and http request

--- shop_app/lib/providers/products.dart
+++ shop_app/lib/providers/products.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'product.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 //ChangeNotifier trabaja con provider esta utiliza VoidCallback para notificar
 class Products with ChangeNotifier {
@@ -61,32 +63,46 @@ class Products with ChangeNotifier {
   Product findById(String id) {
     return _items.firstWhere((element) => element.id == id);
   }
-  void updateProduct (String id, Product newProduct){
-    final prodIndex = _items.indexWhere((prod) =>  prod.id == id);
-    if(prodIndex >=0){
+
+  void updateProduct(String id, Product newProduct) {
+    final prodIndex = _items.indexWhere((prod) => prod.id == id);
+    if (prodIndex >= 0) {
       _items[prodIndex] = newProduct;
       notifyListeners();
-    }else{
+    } else {
       print("...");
     }
-    
+
     notifyListeners();
   }
 
   void addProvider(Product product) {
-    final newProduct = Product(
-      title: product.title,
-      description: product.description,
-      price: product.price,
-      imageUrl: product.imageUrl, 
-      id: DateTime.now().toString(), 
-    );
-    _items.add(newProduct);
-    notifyListeners();
+    const url = 'https://flutter-update-59d81.firebaseio.com/products.json';
+    http
+        .post(url,
+            body: json.encode({
+              'title': product.title,
+              'description': product.description,
+              'imageUrl': product.imageUrl,
+              'price': product.price,
+              'isFavorite': product.isFavorite
+            }))
+        .then((resp) {
+      print(json.decode(resp.body)['name']);
+      final newProduct = Product(
+        title: product.title,
+        description: product.description,
+        price: product.price,
+        imageUrl: product.imageUrl,
+        id:json.decode(resp.body)['name'],
+      );
+      _items.add(newProduct);
+      notifyListeners();
+    });
   }
 
-  void deleteProduct(String id){
-    _items.removeWhere((prop) => prop.id == id); 
+  void deleteProduct(String id) {
+    _items.removeWhere((prop) => prop.id == id);
     notifyListeners();
   }
 }
